0fe00e3f57ad51bb885443caa6023e1e8e8fdd37
MrAndre <48051685+MrAndre18@users.noreply.github.com>
2023-07-27 16:48:48 +0300

    Update

--- change PDF.py
+++ change PDF.py
@@ -3,6 +3,7 @@ import re
 from datetime import datetime
 from langdetect import detect
 from translate import Translator
+import fitz
 
 ################################
 # Редактирование инфо о законе #
@@ -73,8 +74,8 @@ def get_law_name(doc):
   return law_name.strip()
 
 def get_law_data(doc):
-  text = translate_text('||'.join(doc))
-  text = text.split('||')
+  text = translate_text('|'.join(doc))
+  text = text.split('|')
   document_title = []
   
   for line in text:
@@ -87,11 +88,12 @@ def get_law_data(doc):
   law_tipe = get_law_tipe(document_title)
   law_name = get_law_name(document_title)
   
-  processing_law_data(law_number, law_date, law_tipe, law_name)
+  #(!!!) РАССКОММЕНТИРОВАТЬ НА БИЛДЕ
+  #processing_law_data(law_number, law_date, law_tipe, law_name)
 
 def processing_law_data(law_number, law_date, law_tipe, law_name):
   # Обработчик информации о законе
-  # Тут будет запись в БД
+  #(!!!) Тут будет запись в БД
   print('law_number:', law_number)
   print('law_date:', law_date)
   print('law_tipe:', law_tipe)
@@ -107,25 +109,19 @@ def remove_empty_lines(lines):
 
 def remove_first_last_lines(lines, phrase, pages_count):
   """Удаляет ненужные строки"""
-  for line in lines:
+  for index, line in enumerate(lines):
     if phrase in line: # Проверяем, содержит ли строка заданную фразу
       line = line.split()[1:]
       line = ' '.join(line)
       line = re.sub('/' + str(pages_count), '||', line).strip()
       line = line.split('||')[1:]
       line = ' '.join(line)
-  ######
-  
-  ######
-  
-  ###### ДОДЕЛАТЬ ИЗМЕНЕНИЕ В ОБЩЕМ МАССИВЕ
-  
-  ######
-  
-  ######
-  print('lines', lines)
-    
-  return lines
+      lines[index] = line
+      
+    line = line.strip()  # Убираем лишние пробелы в начале и конце каждой строки
+    lines[index] = line
+
+  return lines[1:]
 
 def join_page_text(page, lines):
   """Обновляет текст страницы"""
@@ -133,6 +129,39 @@ def join_page_text(page, lines):
   #page.merge_page(page_content)
   
   return page_content
+
+def flags_decomposer(flags):
+    """Make font flags human readable."""
+    l = []
+    if flags & 2 ** 4:
+        l.append("bold")
+    return ", ".join(l)
+
+def extract_text_from_pdf(file_path):
+  doc = fitz.open(file_path)
+  result_doc = []
+  
+  for page in doc:
+    doc_page = []
+    blocks = page.get_text("dict", flags=11)["blocks"]
+    
+    for b in blocks:  # iterate through the text blocks
+      paragraph = []
+      
+      for l in b["lines"]:  # iterate through the text lines
+        for s in l["spans"]:  # iterate through the text spans
+          if s["flags"] == 20:
+            # Жирный шрифт оборачиваем в тег <b>
+            paragraph.append('<b>'+ s["text"].strip() +'</b>')
+          else:
+            paragraph.append(s["text"].strip())
+            
+      paragraph = " ".join(paragraph)
+      doc_page.append(paragraph)
+      
+    result_doc.append(doc_page)
+    
+  return result_doc
 
 ###################
 # Главная функция #
@@ -144,6 +173,13 @@ def process_pdf_file(input_file_path, output_file_path):
   pdf_file = open(input_file_path, 'rb')
   pdf_reader = PyPDF2.PdfReader(pdf_file)
   
+  doc = extract_text_from_pdf(input_file_path)
+  
+  # Обходим каждую страницу
+  for page in doc:
+    page = page[2:]
+    print(page)
+  
   # Обходим каждую страницу
   for page_num in range(len(pdf_reader.pages)):
     page = pdf_reader.pages[page_num]
@@ -151,27 +187,27 @@ def process_pdf_file(input_file_path, output_file_path):
     
     non_empty_lines = remove_empty_lines(lines)
     cleared_page = remove_first_last_lines(non_empty_lines, 'https://', len(pdf_reader.pages))
-    # changed_page = join_page_text(page, non_empty_lines)
+    non_empty_lines = remove_empty_lines(cleared_page)
     
     if page_num == 0:
-      document_title = cleared_page[:4]
-      #print(document_title)
+      document_title = non_empty_lines[:4]
       
-      #get_law_data(document_title)
+      get_law_data(document_title)
     
-    # print(cleared_page)
-    # print('\n#############################################')
-    # print('#############################################')
-    # print('#############################################\n')
+    changed_page = join_page_text(page, non_empty_lines)
+    
+    
     
     # pdf_writer = PyPDF2.PdfWriter()
     # pdf_writer.add_page(changed_page)
     # output_file = open(output_file_path, 'wb')
     # pdf_writer.write(output_file)
     # output_file.close()
+    
+  pdf_file.close()
 
 # Пути к файлам
-input_pdf = "Abu Dhabi/Direction 1/Federal_Decree_Law_№_32_of_2021_regarding_trading_companies.pdf"
+input_pdf = "Abu Dhabi/Direction 1/Ministerial_Resolution_№_71_of_1989_Regarding_the_procedures_for.pdf"
 output_pdf = 'Result PDF/Result.pdf'
 
 # Используем функцию process_pdf_file для обработки файла PDF
